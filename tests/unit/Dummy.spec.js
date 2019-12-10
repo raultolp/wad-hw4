373c1a9fd0414785bffdc6098c8728a8933faddd
RenetRamman <44438131+RenetRamman@users.noreply.github.com>
2019-12-10 19:21:54 +0200

    Add files via upload

--- tests/unit/Dummy.spec.js
+++ tests/unit/Dummy.spec.js
@@ -2,6 +2,11 @@ import { mount } from '@vue/test-utils'
 
 describe('Dummy test', () => {
 
+    it('Check if list is empty, should display text', () => {
+        const wrapper = mount(List)
+        expect(wrapper.html()).toContain('Add your first Todo task')
+    })
+
 
     it('Check if 1 equals 1', () => {
         expect(1).toEqual(1)
